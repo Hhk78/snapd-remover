f9a2bf73848e408d00442df443a7994e1398c1d0
liberteryen <84645312+liberteryen@users.noreply.github.com>
2026-03-25 17:59:43 +0000

    Update remove-snapd.sh

--- remove-snapd.sh
+++ remove-snapd.sh
@@ -1,8 +1,11 @@
-sudo cp zero /usr/bin/snap
-sudo cp zero /usr/bin/snapctl
-sudo cp zero /usr/bin/snapfuse
-sudo cp zero /usr/bin/ubuntu-core-launcher
-sudo chmod +x /usr/bin/snap
-sudo chmod +x /usr/bin/snapctl
-sudo chmod +x /usr/bin/snapfuse
-sudo chmod +x /usr/bin/ubuntu-core-launcher
+sudo rm /usr/bin/snap
+sudo rm /usr/bin/snapctl
+sudo rm /usr/bin/snapfuse
+sudo rm /usr/bin/ubuntu-core-launcher
+sudo ln -s /bin/true /usr/bin/snap
+sudo ln -s /bin/true /usr/bin/snapctl
+sudo ln -s /bin/true /usr/bin/snapfuse
+sudo ln -s /bin/true /usr/bin/ubuntu-core-launcher
+wget https://github.com/liberteryen/snapd-remover/releases/download/forked/snapd_99999-noupdate_all.deb -O snapd.deb
+sudo dpkg -i snapd.deb
+sudo apt install --fix-broken
